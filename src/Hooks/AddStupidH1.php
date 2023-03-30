9f3a2f6f71ad1dd27f35e5dbbe28eca7bb126d0f
Marcus Kober <marcus.kober@gmail.com>
2023-03-30 09:18:10 +0200

    :ok_hand: IMPROVE: Add type hint

--- src/Hooks/AddStupidH1.php
+++ src/Hooks/AddStupidH1.php
@@ -9,7 +9,7 @@ use PhpAttributes\Attributes\Action;
 class AddStupidH1
 {
     #[Action('the_content', 10)]
-    public function addH1($content): void
+    public function addH1(string $content): void
     {
         ?>
         <h1>Stupid H1</h1>
